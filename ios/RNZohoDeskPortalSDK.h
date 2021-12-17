f76ee12ce7e0057c9684fd6781f0f676bc88c45f
Joshua Smith <3375071+josmithua@users.noreply.github.com>
2021-12-17 17:03:12 -0400

    fixes ios build for expo SDK 44

--- ios/RNZohoDeskPortalSDK.h
+++ ios/RNZohoDeskPortalSDK.h
@@ -6,17 +6,8 @@
 //  Copyright © 2020 Facebook. All rights reserved.
 //
 
-#if __has_include("RCTEventEmitter.h")
-#import "RCTEventEmitter.h"
-#else
 #import <React/RCTEventEmitter.h>
-#endif
-
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
 #import <React/RCTBridgeModule.h>
-#endif
 
 typedef NS_ENUM(NSInteger, RNZDThemeType) {
 /// Light theme
