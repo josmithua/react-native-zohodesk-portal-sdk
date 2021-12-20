5b59ff75c623385452fc097721de1370b12f0873
Joshua Smith <3375071+josmithua@users.noreply.github.com>
2021-12-20 11:22:14 -0400

    remove other header maps that break expo 44 builds

--- ios/RNZDPortalHome.h
+++ ios/RNZDPortalHome.h
@@ -6,11 +6,7 @@
 //  Copyright © 2020 Facebook. All rights reserved.
 //
 
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
 #import <React/RCTBridgeModule.h>
-#endif
 
 @interface RNZDPortalHome : NSObject <RCTBridgeModule>
 
